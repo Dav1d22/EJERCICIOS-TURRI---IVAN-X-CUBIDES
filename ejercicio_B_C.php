232e682dd64098a7f49339d81e2b7fa12fe503f7
Dav1d22 <fierro.david2206@gmail.com>
2025-08-26 21:05:04 -0500

    v3

--- ejercicio_B_C.php
+++ ejercicio_B_C.php
@@ -23,7 +23,7 @@
 
         for($i =1; $i <10; $i++){
             $resultado=$numero*$i;
-            echo $numero, "x", $i, "=", $resultado, "<br>" ; 
+            echo $numero, "x", $i, "=", $resultado, "<br>"; 
             
         }
 
